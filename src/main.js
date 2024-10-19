d51f83e5bbf8d7ad49d419ee76ae0a9f43ffb789
tricker-ems <bantillocj1@gmail.com>
2024-10-19 20:33:25 +0800

    configuring folders

--- src/main.js
+++ src/main.js
@@ -16,6 +16,10 @@ import router from './router'
 const app = createApp(App)
 
 const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+      },
+    
     components,
     directives
   })
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+
+</script>
+
+<template>
+  <!-- toggle switch app bar login main -->
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-3" color="teal-darken-3">
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :prepend-icon="
+            theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+          "
+          text="Toggle Theme"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6" class="mx-auto">
+              <v-card
+                class="mx-auto"
+                prepend-icon="mdi-account-alert"
+                subtitle="Login"
+              >
+                <template v-slot:title>
+                  <span class="font-weight-black">Welcome to Jobify</span>
+                </template>
+
+                <v-card-text class="bg-surface-light pt-4">
+                  <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Email" variant="outlined"></v-text-field>
+
+                    <v-text-field
+                      label="Password"
+                      type="password"
+                      variant="outlined">
+                    </v-text-field>
+
+                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+
+      <v-footer color="teal-darken-3" app
+        >2024 - <strong>Jobify</strong>
+      </v-footer>
+    </v-app>
+  </v-responsive>
+  <!-- toggle switch app bar login end-->
+</template>
