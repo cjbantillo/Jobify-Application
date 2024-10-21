aadb9853841388da3be17518659c95f59fbf3b20
tricker-ems <bantillocj1@gmail.com>
2024-10-22 07:00:17 +0800

    Applayout add on template

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,72 +1,46 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
-
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <!-- toggle switch app bar login main -->
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" color="teal-darken-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-16">
+          <v-card
+            class="mx-auto"
+            prepend-icon="mdi-login-variant"
+            subtitle="Login"
+          >
+            <template v-slot:title>
+              <span class="font-weight-black">Welcome to Jobify</span>
+            </template>
 
-        <v-btn
-          :prepend-icon="
-            theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
-          "
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Email" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto pt-16">
-              <v-card
-                class="mx-auto"
-                prepend-icon="mdi-login-variant"
-                subtitle="Login"
-              >
-                <template v-slot:title>
-                  <span class="font-weight-black">Welcome to Jobify</span>
-                </template>
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                >
+                </v-text-field>
 
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Email" variant="outlined"></v-text-field>
+                <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+              </v-form>
 
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined">
-                    </v-text-field>
-
-                    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-                  </v-form>
-                  
-                  <v-divider class="my-5"></v-divider>
-                    <h5 class="text-center">
-                      Don't have an account? <router-link to="register" class="text-primary"> Click here to Register</router-link></h5>
-
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-
-      <v-footer color="teal-darken-3" app
-        >2024 - <strong>Jobify</strong>
-      </v-footer>
-    </v-app>
-  </v-responsive>
-  <!-- toggle switch app bar login end-->
+              <v-divider class="my-5"></v-divider>
+              <h5 class="text-center">
+                Don't have an account?
+                <router-link to="register" class="text-primary">
+                  Click here to Register</router-link
+                >
+              </h5>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
