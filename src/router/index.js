b15d74d491d61a9fa5781b6c5dbaabfc29c1d952
tricker-ems <bantillocj1@gmail.com>
2024-10-22 14:16:08 +0800

    add default home route login as test

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',  // Root path
+      redirect: '/login'  // Redirect to login when accessing the root
+    },
+    {
       path: '/login',  
       name: 'login',
       component: LoginView
