9af19cadbad5a76ad6ad8b22e1dc6b303d4c9f57
jluisbermudez <c9n0thing9@gmail.com>
2024-11-18 19:21:02 +0800

    back btn

--- src/components/layout/RegisterLayout.vue
+++ src/components/layout/RegisterLayout.vue
@@ -1,5 +1,17 @@
-<script setup>
-
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "light",
+    },
+  },
+  methods: {
+    goBack() {
+      window.history.back();
+    },
+  },
+};
 </script>
 
 <template>
@@ -9,9 +21,30 @@
 
       <v-main class="d-flex flex-column flex-grow-1 pt-8" >
         <v-container >
+          <v-btn
+            class="back-btn"
+            @click="goBack"
+          >
+          <v-icon left>mdi-arrow-left</v-icon>
+          </v-btn>
           <slot name="content"></slot>
         </v-container>
       </v-main>
     </v-app>
   </v-responsive>
 </template>
+
+<style>
+  .back-btn{
+    background: transparent;
+    color: #4caf50;
+    border: none;
+    box-shadow: none;
+    font-size: 30px
+  }
+  .back-btn:hover{
+    background: none;
+    border: none;
+    box-shadow: none;
+  }
+</style>
