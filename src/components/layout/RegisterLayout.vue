0afb1c155e1a56fe90a6cc52995177052df87313
cjbantillo <bantillocj1@gmail.com>
2024-12-02 18:08:10 +0800

    refactor: convert LoginLayout.vue and RegisterLayout.vue to script setup syntax

--- src/components/layout/RegisterLayout.vue
+++ src/components/layout/RegisterLayout.vue
@@ -1,37 +1,30 @@
-<script>
-export default {
-  props: {
-    theme: {
-      type: String,
-      default: "light",
-    },
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  theme: {
+    type: String,
+    default: 'light',
   },
-  methods: {
-    goBack() {
-      window.history.back();
-    },
-  },
-};
+})
+
+const goBack = () => {
+  window.history.back()
+}
 </script>
 
 <template>
   <!-- toggle switch app bar login main -->
-  <v-responsive class="border rounded"  >
-    <v-app :theme="theme" class="d-flex flex-column fill-height">
-
-      <v-main class="d-flex flex-column flex-grow-1 pt-8" >
-        <v-container >
-
+  <v-responsive class="border rounded">
+    <v-app :theme="props.theme" class="d-flex flex-column fill-height">
+      <v-main class="d-flex flex-column flex-grow-1 pt-8">
+        <v-container>
           <!-- back button -->
-          <v-btn
-            class="back-btn"
-            @click="goBack"
-          >
-          <v-icon left>mdi-arrow-left</v-icon>
+          <v-btn class="back-btn" @click="goBack">
+            <v-icon left>mdi-arrow-left</v-icon>
           </v-btn>
 
-
-          <!-- comtent slot  -->
+          <!-- content slot -->
           <slot name="content"></slot>
         </v-container>
       </v-main>
@@ -40,16 +33,16 @@ export default {
 </template>
 
 <style>
-  .back-btn{
-    background: transparent;
-    color: #4caf50;
-    border: none;
-    box-shadow: none;
-    font-size: 30px
-  }
-  .back-btn:hover{
-    background: none;
-    border: none;
-    box-shadow: none;
-  }
+.back-btn {
+  background: transparent;
+  color: #4caf50;
+  border: none;
+  box-shadow: none;
+  font-size: 30px;
+}
+.back-btn:hover {
+  background: none;
+  border: none;
+  box-shadow: none;
+}
 </style>
